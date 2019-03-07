a7bbdb257d7c44f0573b30898d06c5b95d53b3cb
Jiang Yinhao <zjjyh96@126.com>
2019-03-06 23:19:38 -0800

    Update sqlite.py to support replicas

--- master/sql/sqlite.py
+++ master/sql/sqlite.py
@@ -24,16 +24,19 @@ def create_table():
     return true
 
 def get_chunkserver(filename):
+    ids = []
+    ips = []
+    ports = []
     conn = sqlite3.connect('master.db')
     c = conn.cursor()
     cursor = c.execute('SELECT cs_id FROM FilenameCsid WHERE file_name=?',(filename,))
     for row in cursor:
-        id = row[0]
-    cursor = c.execute('SELECT ip, port FROM CsidIp WHERE cs_id=?',(id,))
-    for row in cursor:
-        ip = row[0]
-        port = row[1]
-    return id, ip, port
+        ids.append(row[0])
+        cur = c.execute('SELECT ip, port FROM CsidIp WHERE cs_id=?',(row[0]),))
+        for address in cur:
+            ips.append(address[0])
+            ports.append(address[1])
+    return ids, ips, ports
 
 
 def exists(filename):
@@ -56,14 +59,18 @@ def delete_file(filename):
     cursor = c.execute('SELECT filename FROM FilenameCsid WHERE cs_id=?',(id,))
     if cursor.rowcount == 0:
         cur = c.execute('DELETE FROM CsidIp WHERE cs_id=?',(id,))
+    conn.commit()
     conn.close()
     # return True
 
-def insert_file(filenamae, csid, ip, port):
+def insert_file(filenamae, csids):
     conn = sqlite3.connect('master.db')
     c = conn.cursor()
-    cursor = c.execute("INSERT INTO FilenameCsid (file_name, cs_id) VALUES (?,?)",(filenamae,csid))
-    cursor = c.execute("INSERT INTO CsidIp (cs_id, ip, port) VALUES (?,?,?)",(csid,ip,port))
+    cursor = c.execute("INSERT INTO FilenameCsid (file_name, cs_id) VALUES (?,?)",(filenamae,csids[0]))
+    cursor = c.execute("INSERT INTO FilenameCsid (file_name, cs_id) VALUES (?,?)",(filenamae,csids[1]))
+    cursor = c.execute("INSERT INTO FilenameCsid (file_name, cs_id) VALUES (?,?)",(filenamae,csids[2]))
+    conn.commit()
+    # cursor = c.execute("INSERT INTO CsidIp (cs_id, ip, port) VALUES (?,?,?)",(csid,ip,port))
     conn.close()
     # return True
 
@@ -71,5 +78,6 @@ def update_file_chunkserver(filenamae, csid):
     conn = sqlite3.connect('master.db')
     c = conn.cursor()
     cursor = c.execute("UPDATE FilenameCsid SET cs_id = ? WHERE file_name = ?",(filenamae,csid))
+    conn.commit()
     conn.close()
     # return True
