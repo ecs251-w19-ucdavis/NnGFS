ea65c8c3e70b5ce466c84ddda172ca597cca4179
bwang0202@gmail.com <bojun@privatemachines.com>
2019-03-12 08:16:49 -0700

    client bugs

--- client-library/client.py
+++ client-library/client.py
@@ -31,7 +31,7 @@ def get_file_with_chunkserver(filename, address, id):
     cs_port = address[2]
     cs_url = 'http://' + cs_ip + ':' + str(cs_port) + "/chunkserver/?filename=%s&chunk=%d" % (filename, id)
     r = requests.get(cs_url)
-    return r.text
+    return json.loads(r.text)['data']
 
 def put_file_with_chunkserver(filename, address, id, data, backup_str):
     cs_ip = address[1]
@@ -43,13 +43,11 @@ def put_file_with_chunkserver(filename, address, id, data, backup_str):
 
 def get_file(filename, chunk_id):
     addresses = get_chunkserver_with_filename(filename)['chunks']
-    print(addresses)
     file_buffer = get_file_with_chunkserver(filename, addresses[0], chunk_id)
     return file_buffer
 
 def put_file(filename, chunk_id, data):
     addresses = get_chunkserver_with_filename(filename)['chunks']
-    print(addresses)
     backup_str = ','.join(['%d' % x[0] for x in addresses[1:]])
     put_file_with_chunkserver(filename, addresses[0], chunk_id, data, backup_str)
     return len(data)
@@ -59,17 +57,13 @@ def write(path, data, offset, fh):
         if (offset % CS == 0 and len(data) == CS):
             return put_file(path, offset/CS, data)
         # find the chunkserver to be updated
-        end = (offset + len(data)) % CS
+        end = offset % CS + len(data)
         file_buffer = get_file(path, offset/CS)
-        print(file_buffer)
+        print(len(file_buffer), end, offset % CS, len(data))
         if len(file_buffer) < end:
-            new_buffer = "0" * end
-            new_buffer[:len(file_buffer)] = file_buffer
-            file_buffer = new_buffer
+            return put_file(path, offset/CS, file_buffer[:offset % CS] + data)
         else:
-            file_buffer[offset % CS: (offset+len(data)) % CS] = data
-        # write to chunkserver
-        return put_file(path, offset/CS, file_buffer)
+            return put_file(path, offset/CS, file_buffer[:offset % CS] + data + file_buffer[(offset%CS) + len(data):])
     except:
         traceback.print_exc()
         return 0
@@ -81,8 +75,10 @@ def read(path, size, offset, fh):
         end = -1
     return file_buffer[offset % CS: end]
 
-# create_file('dtxt')
-# print(write('dtxt', 'a'*CS, 0, 0))
-print(read('dtxt', CS, 0, 0))
+#print(create_file('dtxt'))
+#print(write('dtxt', 'a'*CS, CS*2, 0))
+
+print(write('dtxt', 'z'*(CS/2 - 1), CS*2 + CS/2, 0))
+#print(read('dtxt', CS, 0, 0))
 
 
